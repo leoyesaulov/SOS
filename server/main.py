a4a9ed050a01a263ee008fbe27961aebe49878d9
Yung Ho <ho.thdung@gmail.com>
2025-09-18 14:54:21 +0200

    add endpoints

---
+++ server/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from state_control import set_state, get_state
+
+app = FastAPI()
+
+@app.post("/set_fill_state/")
+async def set_fill_state(is_full: bool):
+    set_state(is_full)
+
+@app.get("/get_fill_state/")
+async def get_fill_state():
+    return get_state()
