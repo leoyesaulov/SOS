bd7a19b38c18ee282421b4e2b9cee05b073bdcb8
Leo Yesaulov <leo.yesaulov@tum.de>
2025-09-18 14:33:38 +0200

    add get/set for state management

---
+++ server/trace.py
@@ -0,0 +1,31 @@
+import json
+
+state = False # Resembles the state of the system: false if empty (sensor does not detect the bottle constantly), true when otherwise
+
+def swap_state() -> bool:
+    """
+    Switches the current state (see docs for get_state for state)\n
+    Returns: new state
+    """
+    global state
+    state = not state
+    return state
+
+def set_state(new_state) -> bool:
+    """
+    Sets the current state (see docs for get_state for state)\n
+    Params: desired new state\n
+    Returns: the state after update
+    """
+    global state
+    state = new_state
+    return state
+
+def get_state() -> bool:
+    """
+    Resembles the state of the system: false if empty (sensor does not detect the bottle constantly), true when otherwise\n
+    Returns: current state
+    """
+    return state
+
+
