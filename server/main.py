614c24804030d4dec837a5e3ba68854ac52f6ba9
Leo Yesaulov <52819769+leoyesaulov@users.noreply.github.com>
2025-09-18 16:35:29 +0000

    fix state logging part 2, upd type hints in main.py

--- server/main.py
+++ server/main.py
@@ -4,12 +4,14 @@ from state_control import set_state, get_state, state, log_state
 app = FastAPI()
 
 @app.post("/set_fill_state/")
-async def set_fill_state(is_full: bool):
-    if state != is_full:
-      log_state()
+async def set_fill_state(is_full: bool) -> None:
+    old_state = get_state()
+    if old_state != is_full:
+        log_state(is_full)
     set_state(is_full)
 
+
 @app.get("/get_fill_state/")
-async def get_fill_state():
+async def get_fill_state() -> bool:
     return get_state()
 
