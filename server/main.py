c442ab213d5436c7d17144d54452351b20479251
Yung Ho <ho.thdung@gmail.com>
2025-09-18 18:00:46 +0200

    log state when state changes

--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from state_control import set_state, get_state
+from state_control import set_state, get_state, state, log_state
 
 app = FastAPI()
 
 @app.post("/set_fill_state/")
 async def set_fill_state(is_full: bool):
+    if state != is_full:
+      log_state()
     set_state(is_full)
 
 @app.get("/get_fill_state/")
